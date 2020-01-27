6102b0c1e3fadd3530a7e4a179d01ccb39c1db9e
Lluvia Naomy Carmona Avendaño <lluvia_naomy@hotmail.com>
2020-01-27 10:45:53 -0600

    Qss
    Cambios en la UI de ver mesas para el mesero

--- El-Patio/main.cpp
+++ El-Patio/main.cpp
@@ -3,6 +3,7 @@
 #include <QtSql/QSqlDatabase>
 #include <QTranslator>
 #include <QLibraryInfo>
+#include <QFile>
 
 int main(int argc, char *argv[])
 {
@@ -10,9 +11,17 @@ int main(int argc, char *argv[])
     mDatabase = QSqlDatabase::addDatabase("QODBC","Connection");
     mDatabase.setDatabaseName("elpatio");
 
+    QFile stylesheet(":/iconos/resources/Styles.qss");
+
     QApplication a(argc, argv);
+    stylesheet.open(QFile::ReadOnly);
+    QString stylestr = QLatin1String(stylesheet.readAll());
+    a.setStyleSheet(stylestr);
+    stylesheet.close();
+
     MainWindow w;
     w.show();
+
 
     return a.exec();
 }
