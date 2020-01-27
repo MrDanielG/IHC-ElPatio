dbc03fb3d1e86fa7d570ab32e46fe44750f3ab7a
Daniel Garnica Sanchez <thedaniel115@gmail.com>
2020-01-26 22:39:55 -0600

    Conectarse a la BD

--- El-Patio/main.cpp
+++ El-Patio/main.cpp
@@ -1,8 +1,15 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QtSql/QSqlDatabase>
+#include <QTranslator>
+#include <QLibraryInfo>
 
 int main(int argc, char *argv[])
 {
+    QSqlDatabase mDatabase;
+    mDatabase = QSqlDatabase::addDatabase("QODBC","Connection");
+    mDatabase.setDatabaseName("elpatio");
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
