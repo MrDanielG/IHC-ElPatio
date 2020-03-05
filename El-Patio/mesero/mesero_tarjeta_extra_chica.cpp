f160edf8d13f6401c7b88cf1c65646ce0f33eb76
Daniel Garnica Sanchez <thedaniel115@gmail.com>
2020-03-05 02:53:06 -0600

    Set ¨Tarjeta Extra Chica" parent

--- El-Patio/mesero/mesero_tarjeta_extra_chica.cpp
+++ El-Patio/mesero/mesero_tarjeta_extra_chica.cpp
@@ -1,11 +1,14 @@
 #include "mesero_tarjeta_extra_chica.h"
 #include "ui_mesero_tarjeta_extra_chica.h"
+#include "mesero/mesero_editar_platillo.h"
 
-mesero_tarjeta_extra_chica::mesero_tarjeta_extra_chica(QWidget *parent) :
+mesero_tarjeta_extra_chica::mesero_tarjeta_extra_chica(mesero_editar_platillo *parent) :
     QWidget(parent),
     ui(new Ui::mesero_tarjeta_extra_chica)
 {
     ui->setupUi(this);
+
+    this->padre = parent;
 }
 
 mesero_tarjeta_extra_chica::~mesero_tarjeta_extra_chica()
--- El-Patio/mesero/mesero_tarjeta_extra_chica.h
+++ El-Patio/mesero/mesero_tarjeta_extra_chica.h
@@ -3,6 +3,7 @@
 
 #include <QWidget>
 
+class mesero_editar_platillo;
 namespace Ui {
 class mesero_tarjeta_extra_chica;
 }
@@ -12,11 +13,12 @@ class mesero_tarjeta_extra_chica : public QWidget
     Q_OBJECT
 
 public:
-    explicit mesero_tarjeta_extra_chica(QWidget *parent = nullptr);
+    explicit mesero_tarjeta_extra_chica(mesero_editar_platillo *parent = nullptr);
     ~mesero_tarjeta_extra_chica();
 
 private:
     Ui::mesero_tarjeta_extra_chica *ui;
+    mesero_editar_platillo *padre;
 };
 
 #endif // MESERO_TARJETA_EXTRA_CHICA_H
