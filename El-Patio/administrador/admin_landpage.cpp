bcdf5992a21353032f4b55eda38f7afc5f96b141
Daniel Garnica Sanchez <thedaniel115@gmail.com>
2020-03-31 22:06:51 -0600

    UPDATE Admin Landpage

--- El-Patio/administrador/admin_landpage.cpp
+++ El-Patio/administrador/admin_landpage.cpp
@@ -1,11 +1,17 @@
 #include "admin_landpage.h"
 #include "ui_admin_landpage.h"
+#include "administrador/administrador_crud_platillos.h"
+#include "admin_gestion_usuarios.h"
+#include "admin_reportes.h"
+#include "admin_reservaciones.h"
 
-admin_landpage::admin_landpage(QWidget *parent) :
+admin_landpage::admin_landpage(QString _idAdmin, QWidget *parent) :
     QWidget(parent),
     ui(new Ui::admin_landpage)
 {
     ui->setupUi(this);
+
+    this->idAdmin = _idAdmin;
 }
 
 admin_landpage::~admin_landpage()
--- El-Patio/administrador/admin_landpage.h
+++ El-Patio/administrador/admin_landpage.h
@@ -3,6 +3,11 @@
 
 #include <QWidget>
 
+class admin_gestion_usuarios;
+class administrador_crud_platillos;
+class admin_reportes;
+//Falta reservaciones
+
 namespace Ui {
 class admin_landpage;
 }
@@ -12,11 +17,18 @@ class admin_landpage : public QWidget
     Q_OBJECT
 
 public:
-    explicit admin_landpage(QWidget *parent = nullptr);
+    explicit admin_landpage(QString _idAdmin, QWidget *parent = nullptr);
     ~admin_landpage();
 
 private:
     Ui::admin_landpage *ui;
+    QString idAdmin;
+
+    //Paginas de Admin
+    admin_gestion_usuarios *gestionUsuarios;
+    administrador_crud_platillos *crudPlatillos;
+    admin_reportes *reportes;
+
 };
 
 #endif // ADMIN_LANDPAGE_H
